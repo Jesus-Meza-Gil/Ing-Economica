981a3a770e91eb62246cbde265605463d986b92b
Jesus-Meza-Gil <60653507+Jesus-Meza-Gil@users.noreply.github.com>
2021-02-24 16:46:55 -0500

    Add files via upload

---
+++ Ing - Economica/src/ing/economica/IngEconomica.java
@@ -0,0 +1,206 @@
+package ing.economica;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+
+public class IngEconomica extends JFrame{
+    
+    private JPanel panel;
+    private JLabel etiqueta1;
+    private JLabel I;
+    private JLabel i;
+    private JLabel vp;
+    private JLabel vf;
+    private JLabel T;
+    private JButton bot1;
+    private ButtonGroup GrupoBot;
+    private JRadioButton r1;
+    private JRadioButton r2;
+    private JRadioButton r3;
+    private JRadioButton r4;
+    private JRadioButton r5;
+    
+    public IngEconomica(){
+            setSize(500, 500);
+            setTitle("Formulas");
+            setLocationRelativeTo(null);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            
+            Metodos();
+    }
+    
+    private void Metodos (){
+        Panel();
+        Etiquetas();
+        RadioBotones();
+        Botones();
+        
+    }
+    
+    
+    private void Panel(){
+        
+        panel = new JPanel();
+        getContentPane().add(panel);
+        panel.setLayout(null);
+        panel.setBackground(Color.GRAY);
+    }    
+
+    private void Etiquetas(){
+            
+        etiqueta1 =  new JLabel();
+        panel.add(etiqueta1);
+        etiqueta1.setText(" ¿QUE OPERACIÓN DESEA HACER?");
+        etiqueta1.setOpaque(true);
+        etiqueta1.setBounds(60, 20, 350, 20);
+        etiqueta1.setForeground(Color.CYAN);
+        etiqueta1.setFont(new Font("Arial",1,20));
+        etiqueta1.setBackground(Color.GRAY);
+        
+        
+        I = new JLabel();
+        panel.add(I);
+        I.setText( "Hallar el interés");
+        I.setFont(new Font("System",1,16));
+        I.setForeground(Color.CYAN);
+        I.setBounds(45, 70, 300, 20);
+        
+        
+        i = new JLabel();
+        panel.add(i);
+        i.setText( "Hallar la tasa de interes");
+        i.setFont(new Font("System",1,16));
+        i.setForeground(Color.CYAN);
+        i.setBounds(45, 100, 300, 20);
+        
+        vf = new JLabel();
+        panel.add(vf);
+        vf.setText( "Hallar el valor final");
+        vf.setFont(new Font("System",1,16));
+        vf.setForeground(Color.CYAN);
+        vf.setBounds(45, 130, 300, 20);
+        
+        vp = new JLabel();
+        panel.add(vp);
+        vp.setFont(new Font("System",1,16));
+        vp.setForeground(Color.CYAN);
+        vp.setText( "Hallar el valor presente");
+        vp.setBounds(45, 160, 300, 20);
+        
+        T = new JLabel();
+        panel.add(T);
+        T.setText( "Hallar el tiempo");
+        T.setFont(new Font("System",1,16));
+        T.setForeground(Color.CYAN);
+        T.setBounds(45, 190, 300, 20);
+        
+        
+    }
+    private void RadioBotones(){
+        
+        r1 = new JRadioButton();
+        panel.add(r1);
+        r1.setBounds(20, 70, 20, 20);
+        r1.setBackground(Color.DARK_GRAY);
+         
+        r2 = new JRadioButton();
+        panel.add(r2);
+        r2.setBounds(20, 100, 20, 20);
+        r2.setBackground(Color.DARK_GRAY);
+        
+        r3 = new JRadioButton();
+        panel.add(r3);
+        r3.setBounds(20, 130, 20, 20);
+        r3.setBackground(Color.DARK_GRAY);
+        
+        r4 = new JRadioButton();
+        panel.add(r4);
+        r4.setBounds(20, 160, 20, 20);
+        r4.setBackground(Color.DARK_GRAY);
+        
+        r5 = new JRadioButton();
+        panel.add(r5);
+        r5.setBounds(20, 190, 20, 20);
+        r5.setBackground(Color.DARK_GRAY);
+        
+        GrupoBot = new ButtonGroup();
+        GrupoBot.add(r1);
+        GrupoBot.add(r2);
+        GrupoBot.add(r3);
+        GrupoBot.add(r4);
+        GrupoBot.add(r5);
+    }
+    
+    
+    
+    private void Botones(){
+        bot1 = new JButton("SIGUIENTE");
+        panel.add(bot1);
+        bot1.setFont(new Font("System",1,12));
+        bot1.setForeground(Color.WHITE);
+        bot1.setBackground(Color.BLUE);
+        bot1.setBounds(300, 300, 100, 40);
+        bot1.addActionListener(accion);
+        }
+    
+    
+    ActionListener accion = new ActionListener(){
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            
+            if (GrupoBot.isSelected(null)){
+                    JOptionPane.showMessageDialog(panel, "Por favor seleccione alguna opción");
+            }else{
+
+                 if(r1.isSelected()){
+                     Interes inte = new Interes();
+                     inte.setVisible(true);
+                     dispose();
+                 }
+                 if(r2.isSelected()){
+                     Tinteres Ti = new Tinteres();
+                     Ti.setVisible(true);
+                     dispose();
+                 }
+                 if(r3.isSelected()){
+                     ValorFinal vf = new ValorFinal();
+                     vf.setVisible(true);
+                     dispose();
+                 }
+                 if(r4.isSelected()){
+                     ValorPresente vp = new ValorPresente();
+                     vp.setVisible(true);
+                     dispose();
+                 }
+                 if(r5.isSelected()){
+                     Tiempo T = new Tiempo();
+                     T.setVisible(true);
+                     dispose();
+                 }
+            }
+        }
+        
+    };
+    
+      public static void main(String[] args) {
+        IngEconomica ie = new IngEconomica();
+        ie.setVisible(true);
+    }
+    
+    
+}
+
+
+
+    
+
+    
---
+++ Ing - Economica/src/ing/economica/Interes.java
@@ -0,0 +1,166 @@
+package ing.economica;
+//import java.awt.Color;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
+public class Interes extends IngEconomica{
+        
+    private JPanel panel_2;
+    private JLabel eti1;
+    private JLabel eti2;
+    private JLabel eti3;
+    private JLabel eti4;
+    private JTextField cajatexto1;
+    private JTextField cajatexto2;
+    private JButton calcular;
+    private JButton salir;
+    private JButton inicio;
+    
+    public Interes(){
+        setSize(500, 500);
+        setTitle("Interes");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        Metodo2s();
+}
+    
+    private void Metodo2s(){
+        Panel2();
+        Etiquetas();
+        BotonCalcular();
+        BotonSalir();
+        BotonInicio();
+    }
+    
+    private void Panel2(){
+        panel_2 = new JPanel();
+        panel_2.setLayout(null);
+        getContentPane().add(panel_2);
+        panel_2.setBackground(Color.GRAY);
+    }
+    
+    private void Etiquetas(){
+        
+        eti4 =  new JLabel();
+        panel_2.add(eti4);
+        eti4.setText(" HALLAR EL INTERES ");
+        eti4.setOpaque(true);
+        eti4.setBounds(60, 20, 350, 20);
+        eti4.setForeground(Color.CYAN);
+        eti4.setFont(new Font("Arial",1,20));
+        eti4.setBackground(Color.GRAY);
+        
+        
+        eti1 = new JLabel("Ingrese el Valor Final");
+        panel_2.add(eti1);
+        eti1.setFont(new Font("System",1,16));
+        eti1.setForeground(Color.CYAN);
+        eti1.setBounds(50, 70, 200, 20);
+        
+        eti2 = new JLabel("Ingrese el Valor Presente");
+        panel_2.add(eti2);
+        eti2.setFont(new Font("System",1,16));
+        eti2.setForeground(Color.CYAN);
+        eti2.setBounds(50, 150, 200, 20);
+        
+        eti3 = new JLabel("RESULTADO: ");
+        panel_2.add(eti3);
+        eti3.setFont(new Font("System",1,16));
+        eti3.setForeground(Color.CYAN);
+        eti3.setBounds(50, 400, 350, 20);
+        
+        
+        cajatexto1 = new JTextField();
+        panel_2.add(cajatexto1);
+        cajatexto1.setBounds(50, 100, 100, 20);
+        
+        cajatexto2 = new JTextField();
+        panel_2.add(cajatexto2);
+        cajatexto2.setBounds(50, 170, 100, 20);
+    }
+
+    private void BotonCalcular(){
+        
+        calcular = new JButton("CALCULAR");
+        panel_2.add(calcular);
+        calcular.setFont(new Font("System",1,12));
+        calcular.setForeground(Color.WHITE);
+        calcular.setBackground(Color.BLUE);
+        calcular.setBounds(230, 300, 100, 40);
+        calcular.addActionListener(oyente3);
+        
+    }
+    
+    ActionListener oyente3 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+        
+        if(cajatexto1.getText().equals("") || cajatexto2.getText().equals("")){
+            JOptionPane.showMessageDialog(null, "No puede haber campos vacíos", "Error", JOptionPane.WARNING_MESSAGE);
+        } 
+            else{
+            
+                float vf;
+                float vp;
+                float I;
+
+                vf = Integer.parseInt(cajatexto1.getText());
+                vp = Integer.parseInt(cajatexto2.getText());
+
+                I = vf - vp;
+
+                eti3.setText("El Interes es de: " + I + " Pesos");
+            }
+        }
+    };
+    
+    
+    
+    private void  BotonSalir(){
+        
+        salir = new JButton("SALIR");
+        panel_2.add(salir);
+        salir.setFont(new Font("System",1,12));
+        salir.setForeground(Color.WHITE);
+        salir.setBackground(Color.BLUE);
+        salir.setBounds(350, 300, 100, 40);
+        salir.addActionListener(oyente2);
+    }
+    
+    ActionListener oyente2 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            System.exit(0);
+        }
+    };
+    
+    private void  BotonInicio(){
+        
+        inicio = new JButton("INICIO");
+        panel_2.add(inicio);
+        inicio.setFont(new Font("System",1,12));
+        inicio.setForeground(Color.WHITE);
+        inicio.setBackground(Color.BLUE);
+        inicio.setBounds(110, 300, 100, 40);
+        inicio.addActionListener(oyente4);
+    }
+    
+    ActionListener oyente4 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            
+            IngEconomica ieco = new IngEconomica();
+            ieco.setVisible(true);
+            dispose();
+        }
+    };
+    
+}
